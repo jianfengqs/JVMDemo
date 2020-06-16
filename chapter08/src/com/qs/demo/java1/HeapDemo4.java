17a71e560adb1f82d00aa943073d9ba9d7203dd7
zjfqs <jianfengqs@163.com>
2020-06-16 17:03:29 +0800

    fix:调整代码

---
+++ chapter08/src/com/qs/demo/java1/HeapDemo4.java
@@ -0,0 +1,26 @@
+package com.qs.demo.java1;
+
+/**
+ * @Desc:
+ *
+ *  -Xms10m
+ *  -Xmx10m
+ *
+ * @author: jianfeng
+ * @Time: 2020/6/15 17:46
+ */
+public class HeapDemo4 {
+
+    public static void main(String[] args) {
+        System.out.println("start...");
+        try {
+            Thread.sleep(1000000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("end...");
+
+    }
+
+}
